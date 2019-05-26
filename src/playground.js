0c4ec1357df6dfb72bf72026a31b659472bf0610
Wilgert Velinga <wilgert@wilgert.nl>
2019-05-26 11:18:36 +0200

    improve playground.js

--- src/playground.js
+++ src/playground.js
@@ -1,16 +1,18 @@
 const { solarSystemAsArray: planets } = require('./data/solar-system');
 const names = require('./data/names');
 
+// -1. Check if recording is running with correct screen size
+
 // 0. Explain arrow method
 
 // 1. Filter names longer than 7 characters
 
 // 2. Filter names containing the letter f
 
-// 3. To withoutSatellites filter exercise together, explain testing
+// 3. Do withoutSatellites filter exercise together, explain testing
 
 // 4. Let the class do the onlyFastSpinning exercise
-// Send repo to clone on Slack: https://github.com/wilgert/js2w2-exercises
+// Send repository to clone on Slack: https://github.com/wilgert/js2w2-exercises
 
 // 5. Map names to length
 
