55b241f8f166784cf358c45d27f905a84c443b97
Wilgert Velinga <wilgert@wilgert.nl>
2019-09-01 09:47:13 +0200

    improve playground

--- src/playground.js
+++ src/playground.js
@@ -1,29 +1,87 @@
 const { solarSystemAsArray: planets } = require('./data/solar-system');
 const names = require('./data/names');
 
-// -1. Check if recording is running with correct screen size
+// Check if recording is running with correct screen size
 
-// 0. Explain arrow method
+// Go through homework solution
 
-// 1. Filter names longer than 7 characters
+{
+  const fruitArray = ['banana', 'orange', 'apple', 'lime'];
 
-// 2. Filter names containing the letter f
+  const fruitObject = {
+    banana: {
+      color: 'yellow',
+      weight: 200,
+    },
+    orange: {
+      color: 'orange',
+      weight: 150,
+    },
+    apple: {
+      color: 'red',
+      weight: 120,
+    },
+    lime: {
+      color: 'green',
+      weight: 60,
+    },
+  };
 
-// 3. Do withoutSatellites filter exercise together, explain testing
+  for (const fruit of fruitArray) {
+    const color = fruitObject[fruit].color;
+    const weight = fruitObject[fruit].weight;
 
-// 4. Let the class do the onlyFastSpinning exercise
+    console.log(color);
+    console.log(weight);
+  }
+}
+
+// Explain JSON
+
+// Explain arrow method
+
+{
+  // Function Declaration
+  function sumDeclaration(a, b) {
+    return a + b;
+  }
+
+  // Function Expression
+  const sumFunction = function(a, b) {
+    return a + b;
+  };
+
+  // Arrow function with brackets
+  const sumArrow = (a, b) => {
+    return a + b;
+  };
+
+  // Arrow function without brackets
+  const sumArrowNoBrackets = (a, b) => a + b;
+
+  // Arrow function with 1 argument
+  const square = n => n * n;
+}
+
+// Filter names longer than 7 characters
+
+// Filter names containing the letter f
+
+// Do withoutSatellites filter exercise together, explain testing
+
+// Let the class do the onlyFastMoving exercise
 // Send repository to clone on Slack: https://github.com/wilgert/js2w2-exercises
 
-// 5. Map names to length
+// Map names to length
 
-// 6. Map names to first letter only
+// Map names to first letter only
 
-// 7. Map names to lower case
+// Map names to lower case
 
-// 8. Let the class do the circumference exercise
+// Let the class do the circumference exercise
 
-// 9. Reduce names to total number of letters
+// Reduce names to total number of letters
 
-// 10. Reduce names to total number of letter m
+// Reduce names to total number of letter m
 
-// 11. Let the class do the totalMass exercise
+// Let the class do the totalMass exercise
